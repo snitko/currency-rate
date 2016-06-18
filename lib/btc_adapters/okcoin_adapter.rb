355462ab08bc8ab7da6d5befcb04431ef39424a0
Roman Snitko <roman.snitko@gmail.com>
2016-06-19 00:17:49 +0700

    Refactoring: OKCoin gets support for currency pairs

--- lib/btc_adapters/okcoin_adapter.rb
+++ lib/btc_adapters/okcoin_adapter.rb
@@ -1,13 +1,39 @@
 module CurrencyRate
   class OkcoinAdapter < BtcAdapter
 
-    FETCH_URL = 'https://www.okcoin.com/api/ticker.do?ok=1'
+    FETCH_URL = {
+      'ltc_usd' => 'https://www.okcoin.com/api/v1/ticker.do?symbol=ltc_usd',
+      'btc_usd' => 'https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd',
+      'ltc_cny' => 'https://www.okcoin.cn/api/ticker.do?symbol=ltc_cny',
+      'btc_cny' => 'https://www.okcoin.cn/api/ticker.do?symbol=btc_cny'
+    }
 
     def rate_for(to,from)
       super
       raise CurrencyNotSupported if currency_code != 'USD'
       rate = get_rate_value_from_hash(@rates, 'ticker', 'last')
       rate_to_f(rate)
+    end
+
+    def rate_for(to,from)
+      super
+      rate = rate_to_f(currency_pair_rate(to,from))
+      invert_rate(to,from,rate)
+    end
+
+    def currency_pair_rate(currency1, currency2)
+      rate = @rates["#{currency1.downcase}_#{currency2.downcase}"] || @rates["#{currency2.downcase}_#{currency1.downcase}"]
+      raise CurrencyNotSupported unless rate
+      rate['ticker']['last']
+    end
+
+    # Because OKCoin has LTC
+    def invert_rate(to,from,rate)
+      if ['BTC', 'LTC'].include?(to)
+        1/rate.to_f
+      else
+        rate
+      end
     end
 
   end
--- spec/lib/btc_adapters/okcoin_adapter_spec.rb
+++ spec/lib/btc_adapters/okcoin_adapter_spec.rb
@@ -15,8 +15,16 @@ RSpec.describe CurrencyRate::OkcoinAdapter do
   end
 
   it "finds the rate for currency code" do
-    expect(@exchange_adapter.rate_for('USD')).to be_kind_of(Float)
-    expect( -> { @exchange_adapter.rate_for('FEDcoin') }).to raise_error(CurrencyRate::Adapter::CurrencyNotSupported)
+    expect(@exchange_adapter.rate_for('USD', 'BTC')).to eq(769.05)
+    expect(@exchange_adapter.rate_for('BTC', 'USD')).to eq(0.0013003055718093753)
+    expect(@exchange_adapter.rate_for('CNY', 'BTC')).to eq(5113.0)
+    expect(@exchange_adapter.rate_for('BTC', 'CNY')).to eq(0.00019557989438685703)
+
+    expect(@exchange_adapter.rate_for('USD', 'LTC')).to eq(5.661)
+    expect(@exchange_adapter.rate_for('LTC', 'USD')).to eq(0.17664723547076489)
+    expect(@exchange_adapter.rate_for('CNY', 'LTC')).to eq(37.68)
+    expect(@exchange_adapter.rate_for('LTC', 'CNY')).to eq(0.02653927813163482)
+    expect( -> { @exchange_adapter.rate_for('FEDcoin', 'USD') }).to raise_error(CurrencyRate::Adapter::CurrencyNotSupported)
   end
 
   it "raises exception if rate is nil" do
@@ -28,7 +36,7 @@ RSpec.describe CurrencyRate::OkcoinAdapter do
     3.times do |i|
       @exchange_adapter.instance_variable_set :@rates_updated_at, Time.now
       @exchange_adapter.instance_variable_set :@rates, JSON.parse(response[i])
-      expect( -> { @exchange_adapter.rate_for('USD') }).to raise_error(CurrencyRate::Adapter::CurrencyNotSupported)
+      expect( -> { @exchange_adapter.rate_for('FEDcoin', 'BTC') }).to raise_error(CurrencyRate::Adapter::CurrencyNotSupported)
     end
   end
 
