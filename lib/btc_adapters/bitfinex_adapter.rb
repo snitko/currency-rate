471c559f11b52fd71318b19eb7c35a1573775a9e
Roman Snitko <roman.snitko@gmail.com>
2016-06-20 14:00:01 +0700

    Add Bitfinex adapter

---
+++ lib/btc_adapters/bitfinex_adapter.rb
@@ -0,0 +1,30 @@
+module CurrencyRate
+  class BitfinexAdapter < BtcAdapter
+
+    FETCH_URL = {
+      'btc_usd' => 'https://api.bitfinex.com/v1/pubticker/btcusd',
+      'ltc_usd' => 'https://api.bitfinex.com/v1/pubticker/ltcusd'
+    }
+
+    def rate_for(from,to)
+      super
+      rate = rate_to_f(currency_pair_rate(to,from))
+      invert_rate(from,to,rate)
+    end
+
+    def currency_pair_rate(currency1, currency2)
+      rate = @rates["#{currency1.downcase}_#{currency2.downcase}"] || @rates["#{currency2.downcase}_#{currency1.downcase}"]
+      raise CurrencyNotSupported unless rate
+      rate['last_price']
+    end
+
+    def invert_rate(from,to,rate)
+      if ['BTC', 'LTC'].include?(to)
+        1/rate.to_f
+      else
+        rate
+      end
+    end
+
+  end
+end
---
+++ spec/lib/btc_adapters/bitfinex_adapter_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+RSpec.describe CurrencyRate::BitfinexAdapter do
+
+  before :all do
+    VCR.insert_cassette 'exchange_rate_adapters/btc_adapters/bitfinex_adapter'
+  end
+
+  after :all do
+    VCR.eject_cassette
+  end
+
+  before(:each) do
+    @exchange_adapter = CurrencyRate::BitfinexAdapter.instance
+  end
+
+  it "finds the rate for currency code" do
+    expect(@exchange_adapter.rate_for('BTC', 'USD')).to eq(763.0)
+    expect(@exchange_adapter.rate_for('USD', 'BTC')).to eq(0.001310615989515072)
+    expect(@exchange_adapter.rate_for('LTC', 'USD')).to eq(5.5105)
+    expect(@exchange_adapter.rate_for('USD', 'LTC')).to eq(0.1814717357771527)
+    expect( -> { @exchange_adapter.rate_for('FEDcoin', 'USD') }).to raise_error(CurrencyRate::Adapter::CurrencyNotSupported)
+  end
+
+  it "raises exception if rate is nil" do
+    json_response_1 = '{}'
+    json_response_2 = '{"mid":"762.495","bid":"762.49","ask":"762.5","last_price":"762.5","low":"743.1","high":"775.0","volume":"25680.26724445","timestamp":"1466405569.857112499"}'
+    json_response_3 = '{"mid":"762.495","bid":"762.49","ask":"762.5","last_price":"762.5","low":"743.1","high":"775.0","volume":"25680.26724445","timestamp":"1466405569.857112499"}'
+    uri_mock = double('uri mock')
+    allow(uri_mock).to receive(:read).with(read_timeout: 4).and_return(json_response_1, json_response_2, json_response_3)
+    allow(URI).to      receive(:parse).and_return(uri_mock)
+    3.times do
+      @exchange_adapter.fetch_rates!
+      expect( -> { @exchange_adapter.rate_for('FEDcoin', 'BTC') }).to raise_error(CurrencyRate::Adapter::CurrencyNotSupported)
+    end
+  end
+
+end
