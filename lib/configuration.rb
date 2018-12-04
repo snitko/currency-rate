eb514e1f524bcdbbd928553446e9985a7115611b
dsalamau <salamaudavid@gmail.com>
2018-12-04 13:44:42 +0300

    New: Add crypto-crypto fetch opportunity

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -9,17 +9,17 @@ module CurrencyRate
     attr_accessor :read_timeout
 
     def initialize
-      @api_keys     = { }
-      @file_storage = { path: "" }
-      @logger       = {
-        device: $stdout,
-        level:  :info,
-        formatter: nil,
-       }
+      @api_keys         = { }
+      @file_storage     = { path: "" }
+      @logger           = {
+                            device: $stdout,
+                            level:  :info,
+                            formatter: nil,
+                          }
        @crypto_adapters = CurrencyRate.adapters :crypto
-       @fiat_adapters =   CurrencyRate.adapters :fiat
+       @fiat_adapters   = CurrencyRate.adapters :fiat
        @connect_timeout = 4
-       @read_timeout = 4
+       @read_timeout    = 4
     end
   end
 end
